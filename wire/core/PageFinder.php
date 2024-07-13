5be95787a37028d180d8c0eb23aa6ba0d2c83813
Jeff Harris <jeff@jeffharris.us>
2024-07-13 13:42:51 -0400

    Split PageFinder classes.

--- wire/core/PageFinder.php
+++ wire/core/PageFinder.php
@@ -3733,15 +3733,3 @@ class PageFinder extends Wire {
 		throw new PageFinderSyntaxException($message); 
 	}
 }
-
-/**
- * Typehinting class for DatabaseQuerySelect object passed to Fieldtype::getMatchQuery()
- *
- * @property Field $field Original field
- * @property string $group Original group of the field
- * @property Selector $selector Original Selector object
- * @property Selectors $selectors Original Selectors object
- * @property DatabaseQuerySelect $parentQuery Parent database query
- * @property PageFinder $pageFinder PageFinder instance that initiated the query
- */
-abstract class PageFinderDatabaseQuerySelect extends DatabaseQuerySelect { }
